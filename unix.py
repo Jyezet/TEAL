76608a9e3f44130d010dc91ce515d4830c58713b
Jyezet <65241791+Jyezet@users.noreply.github.com>
2023-04-15 18:32:11 -0300

    Create unix.py

---
+++ unix.py
@@ -0,0 +1,10 @@
+def gettime(): # Get current UNIX time, to display to the user when the next batch will be sent 
+  year = datetime.now().year
+  month = datetime.now().month
+  day = datetime.now().day
+  hour = datetime.now().hour
+  minute = datetime.now().minute
+  second = datetime.now().second
+  tempvar = datetime(year, month, day, hour, minute, second)
+  unix = tm.mktime(tempvar.timetuple())
+  return unix
